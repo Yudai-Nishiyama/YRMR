344b6fa7bb010f01e6c8894fdf3cf081f9e3f9a1
MANA <mana.tdlsm1801@gmail.com>
2023-12-09 12:43:51 +0900

    added room-search.blade.php and fixed navbar part inapp.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }}| @yield('title')</title>
-
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -18,16 +18,22 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    {{-- font awesome --}}
+    {{-- FontAwesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+    <!-- Link to Raleway font -->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Link to Bona Nova font -->
+    <link href="https://fonts.googleapis.com/css2?family=Bona Nova:wght@400;700&display=swap" rel="stylesheet">
     {{-- font Bona Nova and Raleway --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bona+Nova:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+
     <style>
-        *{
+        /* *{
             border:1px solid blue;
-        }
+        } */
 
         h1,h2,h3,h4,h5,h6{
             font-family: 'Bona Nova', serif;
@@ -58,6 +64,81 @@
         .img-md{
             height: 258px;
             width: 237px;
+        }
+
+        /* Navbar */
+        .navbar {
+            background-color: #2C462B;
+            font-family: 'Bona Nova', sans-serif;
+        }
+  
+        .navbar-brand img {
+            height: 80px;
+            width: auto; 
+        }
+    
+        .admin-panel {
+            color: #F4BB4B;
+            font-weight: bold;
+            font-size: 48px
+        }
+    
+        .hi-username {
+            color: #fff;
+            font-size: 27px;
+            font-weight: bolder;
+            font-family: 'Raleway', sans-serif;
+        }
+
+        .logout {
+            color: #F4924B;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .logout:hover {
+            color: #F4BB4B;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        /* Admin Controls */
+        .admin-control.row {
+            display: flex;
+            flex-wrap: wrap; /* 画面が狭い場合に要素を折り返す */
+        }
+
+        .list-group {
+            font-family: 'Bona Nova', sans-serif;
+            font-weight: bolder;
+            font-size: 32px;
+            border-radius: 0;
+            background-color: #F4924B;
+            margin-bottom: 0;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            box-shadow: none;
+            border: 0;
+        }
+
+        .list-group-item {
+            background-color: #F4924B;
+            color: #2C462B;
+            border: none;
+            border-bottom: 1px solid #2C462B;
+        }
+
+        .list-group-item:hover { /* カーソルが重なったとき */
+            background-color: #f4914b58;
+            color: #2C462B; 
+        }
+
+        .list-group-item.active { 
+            background-color: #F4BB4B;
+            color: #2C462B; 
+            border: none;
+            border-bottom: 1px solid #2C462B;   
         }
 
     </style>
@@ -116,17 +197,16 @@
                 <a class="navbar-brand d-flex align-items-center me-5" href="{{ url('/') }}">
                     <img src="/images/logo.jpeg" alt="Logo">
                 </a>
-                  <span class="admin-panel me-auto h2 mb-0">Admin Panel</span>
-                  <div class="collapse navbar-collapse" id="myNavbar">
+                <span class="admin-panel me-auto h2 mb-0">Admin Panel</span>
+                <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="navbar-nav ms-auto">
-                          <li class="nav-item">
-                              <span class="username d-block mt-2 me-5">Hi username!</span>
-                          </li>
-                          <li class="nav-item">
-                              <a class="nav-link logout ms-4 me-4" href="#">Log Out</a>
-                          </li>
-                      </ul>
-                  </div>
+                        <li class="nav-item">
+                            <span class="hi-username d-block mt-2 me-5">Hi username!</span>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link logout ms-4 me-4" href="#">Log Out</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
