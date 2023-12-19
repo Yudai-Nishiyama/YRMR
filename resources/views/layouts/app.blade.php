894752b9215fdd0c15fcc20abaf001c8778eb767
MANA <mana.tdlsm1801@gmail.com>
2023-12-19 22:06:23 +0900

    create css file of navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,6 +51,7 @@
     <link href="{{ asset('css/admins/create-a-new-room.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/admins/all-rooms.css') }}">
     <link rel="stylesheet" href={{ asset('/css/style.css') }}>
+    <link rel="stylesheet" href={{ asset('css/admins/navbar.css') }}>
 
 
 </head>
@@ -68,7 +69,7 @@
                 <div class="collapse navbar-collapse" id="myNavbar">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <span class="username d-block mt-2 me-5">Hi username!</span>
+                            <span class="hi-username d-block mt-2 me-5">Hi username!</span>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link logout ms-4 me-4" href="#">Log Out</a>
