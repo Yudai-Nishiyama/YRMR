8bac6e80b3cbeac61df884b775f4efd690777e74
reimi_fujiwara <reimi0216momo@gmail.com>
2023-12-19 21:21:57 +0900

    I made users_table , rooms_table and room_types_table .

---
+++ database/migrations/2023_12_18_124913_create_rooms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('image');
+            $table->unsignedBigInteger('room_type_id');
+            $table->boolean('is_active')->default('true');
+            $table->timestamps();
+            $table->foreign('room_type_id')->references('id')->on('room_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
