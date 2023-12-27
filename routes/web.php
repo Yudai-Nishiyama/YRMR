2ace69670098ea0ecca9dfd46d0eb567121e70a5
reimi_fujiwara <reimi0216momo@gmail.com>
2023-12-27 21:54:50 +0900

    I add a route for homepge

--- routes/web.php
+++ routes/web.php
@@ -78,7 +78,7 @@ Route::get('/admins/cleaners/create_cleaner', [CleanersController::class, 'showC
 Route::get('/cleaner/cleaning_task', [CleanerController::class, 'showCleaningTask'])->name('showTask');
 Route::get('/admin/cleaner/cleaning_task', [CleanersController::class, 'showCleanersPage'])->name('showCleanersPage');
 
-
+Route::get('guest/home',[GuestController::class, 'guesthome'])->name('guesthome');
 Route::get('/guest/room', [GuestController::class, 'guestroom'])->name('guestRoom');
 Route::get('/guest/detail', [GuestController::class, 'roomdetail'])->name('roomDetail');
 
