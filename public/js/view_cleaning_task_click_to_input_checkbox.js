aa6e2302bcd63c21908c1eee23756c9a683582be
YUDAI-PC\21332 <21332001yn@tama.ac.jp>
2024-01-20 12:54:46 +0900

    deleted code on javascript which is no needed

--- public/js/view_cleaning_task_click_to_input_checkbox.js
+++ public/js/view_cleaning_task_click_to_input_checkbox.js
@@ -1,21 +1,3 @@
-// function handleCheckboxClick(taskId, isChecked) {
-//     // Ajaxを使用してデータをサーバーに送信
-//     console.log('Checkbox with id ' + taskId + ' is checked: ' + isChecked);
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('POST', 'http://localhost/phpmyadmin/index.php?route=/sql&db=15batch&table=reservation_task&pos=0', true);
-//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState == 4 && xhr.status == 200) {
-//             console.log(xhr.responseText); // サーバーからの応答をコンソールに表示
-//         }
-//     };
-
-//     var formData = new FormData();
-//     formData.append('task_id', taskId);
-//     formData.append('is_checked', isChecked);
-
-//     xhr.send(formData);
-// }
 
 function handleCheckboxClick(taskId, isChecked,reservation_id) {
     // Logging the status
